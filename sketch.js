efd69d614262eaf867ccd476675c28c37bae110a
An Duong <aduong7@dons.usfca.edu>
2021-03-04 12:29:00 -0800

    changed intro()

--- sketch.js
+++ sketch.js
@@ -170,7 +170,6 @@ drawIntro = function() {
   // fill green color
   fill('#1DB954');
   textSize(24);
-  lineHeight(50);
 
   // parse though string array
   for ( let i = 0; i < intro.length; i++ ) {
